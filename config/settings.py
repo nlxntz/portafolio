d5fd7715a73a1dc219e039454a28ac5a2a48250c
尼古拉斯 <lintznicolas2@gmail.com>
2025-10-26 13:28:59 -0300

    cambios

--- config/settings.py
+++ config/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
